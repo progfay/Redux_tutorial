7fca3a8ca9831cf2cfdac6d9930d4c189ce475c4
progfay <shun1856shun@gmail.com>
2018-07-03 23:58:34 +0900

    fix: invalid new line char code

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './reducers'
 import App from './components/App'
-​
+
 const store = createStore(todoApp)
-​
+
 render(
   <Provider store={store}>
     <App />
